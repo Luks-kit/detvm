a2d6fed3335ca5488120ea30b6ea70f56d343cdb
Luks-kit <lushdudeaimboot@gmail.com>
2025-10-31 09:53:50 -0500

    added main.cpp and deltora's cmake

---
+++ deltora/src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "emit.hpp"
+#include "lexer.hpp"
+#include "parser.hpp"
+#include "typecheck.hpp"
+
+
+
+int main () {
+    std::cout << "Hello, World!\n";
+    return 0;
+}

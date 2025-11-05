366868b6466cd34fef63c89a373f633e31bfff9c
Luks-kit <lushdudeaimboot@gmail.com>
2025-11-05 09:05:58 -0600

    stmt fixed

--- deltora/inc/stmt_ast.hpp
+++ deltora/inc/stmt_ast.hpp
@@ -1,23 +1,21 @@
 #pragma once
 #include "expr_ast.hpp"
 
-enum class StmtKind { ExprStmt, VarDecl, If, While, Return, Block };
-DEFINE_NODE_BASE(Stmt, StmtKind)
-DEFINE_NODE_PTR(Stmt)
+DEFINE_NODE_BASE(Stmt, StmtKind { ExprStmt, VarDecl, If, While, Return, Block })
 
-DEFINE_NODE_TYPE(Stmt, StmtKind, ExprStmt, StmtKind::ExprStmt,
+DEFINE_NODE_TYPE(Stmt, ExprStmt, StmtKind::ExprStmt,
     ExprPtr expr;
 )
-DEFINE_NODE_TYPE(Stmt, StmtKind, VarDeclStmt, StmtKind::VarDecl,
+DEFINE_NODE_TYPE(Stmt, VarDeclStmt, StmtKind::VarDecl,
     std::string name;
     ExprPtr initializer;
 )
-DEFINE_NODE_TYPE(Stmt, StmtKind, IfStmt, StmtKind::If,
+DEFINE_NODE_TYPE(Stmt, IfStmt, StmtKind::If,
     ExprPtr condition;
     StmtPtr thenBranch;
     StmtPtr elseBranch;
 )
-DEFINE_NODE_TYPE(Stmt, StmtKind, BlockStmt, StmtKind::Block,
+DEFINE_NODE_TYPE(Stmt, BlockStmt, StmtKind::Block,
     std::vector<StmtPtr> statements;
 )
 
@@ -26,4 +24,4 @@ BEGIN_NODE_TABLE(Stmt)
     NODE_ENTRY(VarDeclStmt)
     NODE_ENTRY(IfStmt)
     NODE_ENTRY(BlockStmt)
-END_NODE_TABLE()
+END_NODE_TABLE(Stmt)
